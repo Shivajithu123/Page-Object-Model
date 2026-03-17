4ad311eac5b1df817094cebbd737b91a738cea43
jithushiva123 <99196035+Shivajithu123@users.noreply.github.com>
2026-03-17 08:30:10 +0530

    Update POMLEARN.java

--- Documents/NetBeansProjects/POMLEARN/src/main/java/com/mycompany/pomlearn/POMLEARN.java
+++ Documents/NetBeansProjects/POMLEARN/src/main/java/com/mycompany/pomlearn/POMLEARN.java
@@ -11,6 +11,6 @@ package com.mycompany.pomlearn;
 public class POMLEARN {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
     }
 }
